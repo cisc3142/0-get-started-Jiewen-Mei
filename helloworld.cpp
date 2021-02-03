18f55847c59fa24188d9a2d21ed52e32b55071ec
Jiewen <jiewen.mei30@gmail.com>
2021-02-02 20:33:10 -0500

    second commit

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-/* code credits to Heidi Gentry-Kolen, https://www.youtube.com/watch?v=eXpPo3PTsRo
+/* I requested a second pull becasue I did not read ahead since I followed instruction line 
+*line. So I made a change on code to mlet me do the second pull.
+* code credits to Heidi Gentry-Kolen, https://www.youtube.com/watch?v=eXpPo3PTsRo
 */
 int main(){
 	std::cout << "Hello world" << std::endl;
